e373c5ab08a3af7524667032f3ae1bb99c229ff4
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-07-29 20:41:57 -0400

    adjust build for new repo

--- simput/build.sh
+++ simput/build.sh
@@ -3,7 +3,7 @@ cd `dirname $0`
 source venv/bin/activate
 mkdir -p parflow
 cp src/*.js parflow/
-python src/model_builder.py -d ../../pf-keys/definitions -o parflow
+python src/model_builder.py -d pf-keys -o parflow
 Simput compile -c parflow/ -o parflow/ -t parflow
 cp parflow/parflow.js ../public/parflow.js
 jq '' < parflow/model.json > parflow/readable.json
