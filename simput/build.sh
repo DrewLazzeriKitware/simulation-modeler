713dced921e77d00f811349a9f73484a1cc702e9
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-08-09 19:37:31 -0400

    Add parflow as submodule

--- simput/build.sh
+++ simput/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd `dirname $0`
 source venv/bin/activate
-mkdir -p parflow
-cp src/*.js parflow/
-python src/model_builder.py -d pf-keys -o parflow
-Simput compile -c parflow/ -o parflow/ -t parflow
-cp parflow/parflow.js ../public/parflow.js
-jq '' < parflow/model.json > parflow/readable.json
+mkdir -p type-parflow
+cp src/*.js type-parflow/
+python src/model_builder.py -d parflow/pf-keys/definitions -o type-parflow
+Simput compile -c type-parflow/ -o type-parflow/ -t parflow
+cp type-parflow/parflow.js ../public/parflow.js
+jq '' < type-parflow/model.json > type-parflow/readable.json
