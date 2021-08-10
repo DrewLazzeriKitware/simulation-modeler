7064cfd72e24b4c83dbaeddbdbfce880981a7bd2
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-08-09 20:29:10 -0400

    Add simput submodule

--- simput/build.sh
+++ simput/build.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
+
+# Run this script where it is stored
 cd `dirname $0`
+
+# Use our model building python virtualenv
 source venv/bin/activate
-mkdir -p type-parflow
-cp src/*.js type-parflow/
-python src/model_builder.py -d parflow/pf-keys/definitions -o type-parflow
-Simput compile -c type-parflow/ -o type-parflow/ -t parflow
-cp type-parflow/parflow.js ../public/parflow.js
-jq '' < type-parflow/model.json > type-parflow/readable.json
+
+# Make room for the simput model and copy files over
+mkdir -p build
+cp type-parflow/src/*.js build/
+
+# Generate model.json from our parflow generator and keys
+python parflow/pf-keys/generators/simput_model.py \
+  -d parflow/pf-keys/definitions \
+  -o build
+
+# Compile model.json with other input files
+Simput compile -c build -o build -t parflow
+
+# Include the newly built module in our app
+cp build/parflow.js ../public/parflow.js
+
+# Make a debug version of the model.json
+jq '' < build/model.json > build/readable.json
--- simput/src/constants.py
+++
@@ -1,9 +0,0 @@
-# Const values
-STRING = 'AnyString'
-BOOL = 'BoolDomain'
-DOUBLE = 'DoubleValue'
-ENUM = 'EnumDomain'
-INT = 'IntValue'
-MANDATORY = 'MandatoryValue'
-
-TYPES = [STRING, BOOL, DOUBLE, ENUM, INT]
