3257151bfde4c7d834244fdad603c43acc1690ee
Drew Lazzeri <drew.lazzeri@kitware.com>
2021-10-13 17:29:57 -0600

    Save files in filedb

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 import argparse
+import random
 import sys
 import pfweb
 import yaml
 import time
 
+from pprint import pprint
 from ArgumentValidator import ArgumentValidator
 
 # -----------------------------------------------------------------------------
@@ -16,7 +18,15 @@ if "--virtual-env" in sys.argv:
     exec(open(virtualEnv).read(), {"__file__": virtualEnv})
 
 # -----------------------------------------------------------------------------
-from trame import start, change, update_state, get_cli_parser, enable_module
+from trame import (
+    start,
+    change,
+    update_state,
+    get_cli_parser,
+    enable_module,
+    trigger,
+    get_state,
+)
 from trame.layouts import SinglePage
 from trame.html import vuetify, Div
 
@@ -32,19 +42,43 @@ layout.toolbar.children += [
     '<NavigationDropDown v-model="currentView" :views="views"/>',
     vuetify.VSpacer(),
 ]
-layout.content.children += [
-    '<FileDatabase :files="files" v-model="currentFileIndex" />'
-]
+file_database = """
+<FileDatabase 
+  :files="files" 
+  v-model="selectedFile"
+  @newFile="trigger('newFile')"
+  @selectFile="trigger('selectFile', $event)"
+  @removeFile="trigger('removeFile', $event)"
+  @downloadFile="trigger('downloadFile')"
+  @resetFile="trigger('resetFile')"
+/>
+"""
+layout.content.children += [file_database]
 
 
 @change("currentView")
 def logView(currentView, **kwargs):
-    print(currentView)
+    pprint(currentView)
 
 
-@change("files")
-def logFiles(files, **kwargs):
-    print(files)
+@change("selectedFile")
+def changeCurrentFile(selectedFile, **kwargs):
+    (work_dir, files) = get_state("work_dir", "files")
+
+    # This might be a new file
+    file_id = selectedFile.get("id")
+    if not file_id:
+        file_id = work_dir + str(random.getrandbits(32))
+        selectedFile = {**selectedFile, "id": file_id, "path": work_dir + "/" + file_id}
+
+    # We don't want to keep file data in memory
+    if selectedFile.get("file"):
+        with open(selectedFile.get("path"), "w") as current:
+            current.write(selectedFile.file.read())
+    selectedFile["file"] = None
+
+    update_state("selectedFile", selectedFile)
+    update_state("files", {**files, file_id: selectedFile})
 
 
 # -----------------------------------------------------------------------------
@@ -65,33 +99,34 @@ if __name__ == "__main__":
             "Solver",
             "Project Generation",
         ],
-        "files": [
-            {
-                "name": "MyIndicator",
-                "description": "This is my indicator. I made it. There are many like it, but this one is mine.",
-                "origin": "/oldDrive/oldFolder/originalProject",
-                "path": "/opt/fileDatabases/filedb1",
-                "size": 672716,
-                "dateModified": time.time(),
-                "dateUploaded": time.time(),
-                "type": "file",
-                "gridSize": [50, 50, 2],
-                "category": "Indicator",
-            },
-            {
-                "name": "Rain Forcing",
-                "description": "This simulates heavy rain across the entire surface. It was made by...",
-                "origin": "/oldDrive/oldFolder/otherProject",
-                "path": "/opt/fileDatabases/filedb1",
-                "size": 5321298,
-                "dateModified": time.time(),
-                "dateUploaded": time.time(),
-                "type": "zip",
-                "gridSize": None,
-                "category": "CLM",
-            },
-        ],
-        "currentFileIndex": 0,
+        "files": {},
+        ##"files": {
+        ##    "key1": {
+        ##        "name": "MyIndicator",
+        ##        "description": "This is my indicator. I made it. There are many like it, but this one is mine.",
+        ##        "origin": "/oldDrive/oldFolder/originalProject",
+        ##        "path": "/opt/fileDatabases/filedb1",
+        ##        "size": 672716,
+        ##        "dateModified": time.time(),
+        ##        "dateUploaded": time.time(),
+        ##        "type": "file",
+        ##        "gridSize": [50, 50, 2],
+        ##        "category": "Indicator",
+        ##    },
+        ##    "key2": {
+        ##        "name": "Rain Forcing",
+        ##        "description": "This simulates heavy rain across the entire surface. It was made by...",
+        ##        "origin": "/oldDrive/oldFolder/otherProject",
+        ##        "path": "/opt/fileDatabases/filedb1",
+        ##        "size": 5321298,
+        ##        "dateModified": time.time(),
+        ##        "dateUploaded": time.time(),
+        ##        "type": "zip",
+        ##        "gridSize": None,
+        ##        "category": "CLM",
+        ##    },
+        ##},
+        "selectedFile": None,
     }
 
     parser = get_cli_parser()
