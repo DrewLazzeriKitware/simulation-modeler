b10fe040ffc04b2db0f8f9120944df5dcff752c6
Drew Lazzeri <drew.lazzeri@kitware.com>
2022-01-05 14:55:44 -0700

    Remove unused vue components

--- component_lib/src/components/ProjectGeneration/script.js
+++
@@ -1,5 +0,0 @@
-export default {
-  name: 'ProjectGeneration',
-  props: ['validation', 'validateOn'],
-  inject: ['trigger'],
-};
--- component_lib/src/use.js
+++ component_lib/src/use.js
@@ -1,32 +1,20 @@
 import NavigationDropDown from './components/NavigationDropDown';
 import SimputStandin from './components/SimputStandin';
 import SimulationType from './components/SimulationType';
-import OverlayDatabaseErrors from './components/OverlayDatabaseErrors';
 import FileDatabase from './components/FileDatabase';
-import Domain from './components/Domain';
-import BoundaryConditions from './components/BoundaryConditions';
-import SubSurface from './components/SubSurface';
 import Solver from './components/Solver';
-import ProjectGeneration from './components/ProjectGeneration';
 
-import SimputDomain from './components/SimputDomain';
 import SimputSolver from './components/SimputSolver';
 
 const components = {
   NavigationDropDown,
   SimulationType,
   SimputStandin,
-  OverlayDatabaseErrors,
   FileDatabase,
-  Domain,
-  BoundaryConditions,
-  SubSurface,
   Solver,
-  ProjectGeneration,
 
   // Simput components
   SimputSolver,
-  SimputDomain,
 };
 
 export default {
